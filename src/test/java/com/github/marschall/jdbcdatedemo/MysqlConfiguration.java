c8d7f0419ee1545092a430f6754e528b6b56b7ba
Philippe Marschall <philippe.marschall@gmail.com>
2019-02-24 12:45:37 +0100

    update

--- src/test/java/com/github/marschall/jdbcdatedemo/MysqlConfiguration.java
+++ src/test/java/com/github/marschall/jdbcdatedemo/MysqlConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
+import com.mysql.cj.log.Slf4JLogger;
+
 @Configuration
 public class MysqlConfiguration {
 
@@ -16,7 +18,7 @@ public class MysqlConfiguration {
     String userName = System.getProperty("user.name");
     String database = userName;
     // https://dev.mysql.com/doc/connector-j/6.0/en/connector-j-reference-configuration-properties.html
-    dataSource.setUrl("jdbc:mysql://localhost:3306/" + database);
+    dataSource.setUrl("jdbc:mysql://localhost:3306/" + database + "?useSSL=false&allowPublicKeyRetrieval=true&logger=" + Slf4JLogger.class.getName());
     dataSource.setUsername(userName);
     dataSource.setPassword(userName);
     return dataSource;
